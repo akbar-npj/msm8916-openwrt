1c249a8b6bb667ad43e306e0ef1eb18583f8ce06
akbar_npj <akbar.npj@protonmail.com>
2026-09-06 02:38:04 +0545

    feat(pure-software): completely remove hmu05-patch-modem binary patcher

--- packages/msm-firmware-dumper/files/msm-firmware-dumper.sh
+++ packages/msm-firmware-dumper/files/msm-firmware-dumper.sh
@@ -139,11 +139,6 @@ done
 [ -f "$FW/mcfg_sw.mbn" ] && ln -sf "$FW/mcfg_sw.mbn" "$FW/MCFG_SW.MBN" 2>/dev/null || true
 [ -f "$FW/MCFG_SW.MBN" ] && ln -sf "$FW/MCFG_SW.MBN" "$FW/mcfg_sw.mbn" 2>/dev/null || true
 
-# Apply device-specific No-Sleep patch on Generic HMU05
-if [ -x /usr/sbin/hmu05-patch-modem ]; then
-  /usr/sbin/hmu05-patch-modem "$FW" && log "HMU05 modem patch check completed"
-fi
-
 sync
 
 # Unmount and cleanup
